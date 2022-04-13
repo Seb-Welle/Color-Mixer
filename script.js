00967485bfc8249835b7620b5439ff86312ba24e
Sebastian Welle <s-welle@web.de>
2022-04-13 09:50:03 +0200

    feat: adds button with function random color

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const redSlider = document.querySelector("#red");
 const greenSlider = document.querySelector("#green");
 const blueSlider = document.querySelector("#blue");
 const colorValue = document.querySelector("#color-value");
+const btnRandom = document.querySelector("#btn-random");
 
 function setBackgroundColor() {
   const red = rangeValueToHex(redSlider.value);
@@ -21,3 +22,17 @@ function rangeValueToHex(value) {
 }
 
 document.body.addEventListener("input", setBackgroundColor);
+
+btnRandom.addEventListener("click", function () {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (color) {
+      //console.log(color);
+      redSlider.value = color.rgb.r;
+      greenSlider.value = color.rgb.g;
+      blueSlider.value = color.rgb.b;
+      setBackgroundColor();
+    });
+});
